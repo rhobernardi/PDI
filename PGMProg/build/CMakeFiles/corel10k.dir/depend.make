a8df6476b06539094d79776fff1b31a6e4120c5b
rhobernardi <rodrigo.n.bernardi@gmail.com>
2016-05-22 22:11:01 -0300

    Gera linhas por reconhecimento de bordas em um tabuleiro de Xadrez

--- PGMProg/build/CMakeFiles/corel10k.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for corel10k.
-# This may be replaced when dependencies are built.
--- PGMProg/compile.sh
+++
@@ -1,6 +0,0 @@
-mkdir build && cd build
-cmake ..
-#make
-#rm -rf CMakeCache.txt CMakeFiles/ cmake_install.cmake Makefile
-#mv * ..
-#cd .. && rm -rf build
